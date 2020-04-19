e989178bac567a5467cfbc4ef027fa4810330815
Esu Obu <esuobu111@gmail.com>
2020-04-18 20:31:46 -0400

    added create user method

--- momoney-fe/src/app/classes/user/user.ts
+++ momoney-fe/src/app/classes/user/user.ts
@@ -1,2 +1,21 @@
 export class User {
+    userID: number = 0;
+    firstName: String = "";
+    lastName: String = "";
+    ssn: number = 0;
+    dob: String = "";
+    username: String = "";
+    password: String = "";
+    phoneNum: number = 0;
+    email: String = "";
+
+    streetAddress: String = "";
+    city: String = "";
+    state: String = "";
+    zipcode: number;
+
+    securityQuestionId: number = 0;
+    securityQuestion: String = "";
+    securityQuestionAnswer: String = "";
+
 }
--- momoney-fe/src/app/services/user/user.service.ts
+++ momoney-fe/src/app/services/user/user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/momoney/user';
+
+  constructor(private http: HttpClient) { }
+
+  createUser(user: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}`, user)
+  }
+
+  
+
+
+
 }
